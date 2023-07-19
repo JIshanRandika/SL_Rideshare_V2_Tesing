d45e9a05611dc723999ef6236365bca2d839acce
JIshanRandika <jishanrandika@gmail.com>
2023-07-19 16:10:30 +0530

    NIC camera capture

--- src/screens/registerVerifyNICCardScreen.js
+++ src/screens/registerVerifyNICCardScreen.js
@@ -9,10 +9,10 @@ import {
     Modal,
     Alert,
     Pressable,
-    StyleSheet,
+    StyleSheet, Button,
 } from 'react-native';
 import {Colors} from '../constants/colors'
-
+import ImagePicker from 'react-native-image-crop-picker';
 
 export default function RegisterVerifyNICCardScreen({navigation}) {
     const [modalVisible, setModalVisible] = React.useState(false);
@@ -21,12 +21,36 @@ export default function RegisterVerifyNICCardScreen({navigation}) {
         setModalVisible(!modalVisible);
     };
 
+    const defaultPickerOptions = {
+        cropping: true,
+
+    };
+    const [imgSrc, setImgSrc] = React.useState(null);
+
+    const recognizeFromCamera = async (options = defaultPickerOptions) => {
+        try {
+            const image = await ImagePicker.openCamera(options);
+            setImgSrc({uri: image.path});
+            setModalVisible(true);
+
+        } catch (err) {
+            if (err.message !== 'User cancelled image selection') {
+                console.error(err);
+            }
+        }
+    };
+
     const styles = StyleSheet.create({
         centeredView: {
             flex: 1,
             justifyContent: 'center',
             alignItems: 'center',
             marginTop: 22,
+        },
+        image: {
+            margin: 15,
+            height: 100,
+            width: 200 ,
         },
         modalView: {
             margin: 20,
@@ -51,22 +75,34 @@ export default function RegisterVerifyNICCardScreen({navigation}) {
     });
     return (
         <View style={{flex:1,backgroundColor: Colors.background}}>
-
-            <View style={{flex:5, alignItems:'center'}} >
-                <Image style={{width:340,height:300, borderRadius:20}} source={require('../assets/takePhoto.png')}/>
-            </View>
-            <View style={{flex:5, alignItems:'center',alignContent:'center'}} >
-                <View style={{width:340,backgroundColor:Colors.colorB,borderRadius:30}}>
-                    <Text style={{paddingHorizontal:40,paddingVertical:10,marginTop:20,textAlign:'center',fontWeight:'bold',color:Colors.colorD,fontSize:20}}>
-                        Take a photo of the FRONT of your NIC
-                    </Text>
-                    <Text style={{paddingHorizontal:40,paddingVertical:10,marginBottom:20, textAlign:'center',fontWeight:'bold',color:Colors.colorD,fontSize:17}}>
-                        Position your ID in the indicated zone
-                    </Text>
-
-                </View>
+            {imgSrc &&
+            <View style={{flex: 10, alignItems: 'center'}}>
+                {/*<View>*/}
+                    <Image style={{width:200, height:200, margin:20}} source={imgSrc}/>
+                {/*</View>*/}
 
             </View>
+            }
+            {!imgSrc &&
+                <>
+                    <View style={{flex:5, alignItems:'center'}} >
+                        <Image style={{width:340,height:300, borderRadius:20}} source={require('../assets/takePhoto.png')}/>
+
+                    </View>
+                    <View style={{flex:5, alignItems:'center',alignContent:'center'}} >
+                        <View style={{width:340,backgroundColor:Colors.colorB,borderRadius:30}}>
+                            <Text style={{paddingHorizontal:40,paddingVertical:10,marginTop:20,textAlign:'center',fontWeight:'bold',color:Colors.colorD,fontSize:20}}>
+                                Take a photo of the FRONT of your NIC
+                            </Text>
+                            <Text style={{paddingHorizontal:40,paddingVertical:10,marginBottom:20, textAlign:'center',fontWeight:'bold',color:Colors.colorD,fontSize:17}}>
+                                Position your ID in the indicated zone
+                            </Text>
+
+                        </View>
+                    </View>
+                </>
+
+            }
             <View style={{flex:2, alignItems:'center'}} >
                 <View style={styles.centeredView}>
                     <Modal
@@ -88,13 +124,21 @@ export default function RegisterVerifyNICCardScreen({navigation}) {
                         </View>
                         <View style={styles.blurredBackground} />
                     </Modal>
-                    <TouchableOpacity onPress={() => setModalVisible(true)} style={{backgroundColor:Colors.colorA,padding:15,alignItems:'center',borderRadius:50,width:340}}>
-                        <Text style={{fontWeight:'bold',color:Colors.colorD}}>Start</Text>
+                    {!imgSrc &&
+                    <TouchableOpacity onPress={() => {
+                        recognizeFromCamera();
+                    }} style={{
+                        backgroundColor: Colors.colorA,
+                        padding: 15,
+                        alignItems: 'center',
+                        borderRadius: 50,
+                        width: 340
+                    }}>
+                        <Text style={{fontWeight: 'bold', color: Colors.colorD}}>Start</Text>
                     </TouchableOpacity>
+                    }
+
                 </View>
-            {/*<TouchableOpacity onPress={()=>navigation.navigate('RegisterStep3')} style={{backgroundColor:Colors.colorA,padding:15,alignItems:'center',borderRadius:50,width:340}}>*/}
-            {/*    <Text style={{fontWeight:'bold',color:Colors.colorD}}>Start</Text>*/}
-            {/*</TouchableOpacity>*/}
             </View>
 
 
